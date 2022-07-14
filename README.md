053f8ec38d5ca26cf809d67a9798db52d880306e
Liang Chenghao <lch_office@foxmail.com>
2022-07-14 17:01:45 +0800

    update: update install.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+# 使用
+
+```shell
+bash -c "$(wget -O- https://raw.githubusercontent.com/bonjourcs/easy-configs-git/main/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-GIT_CONFIG_FILE=~/.gitconfig
+GIT_REPOSITORY_URL=https://github.com/bonjourcs/easy-configs-git
+GIT_CONFIG_FILE=.gitconfig
 MODULE_DIR=gitconf
 
-git clone https://github.com/bonjourcs/easy-configs-git $MODULE
-if [ -e $GIT_CONFIG_FILE ]; then
-	mv $GIT_CONFIG_FILE $GIT_CONFIG_FILE.bak
+timestamp=$(date +%s)
+
+if [ -d $MODULE_DIR ] || [ -f $MODULE_DIR ]; then
+	mv $MODULE_DIR $MODULE_DIR.$timestamp
 fi
-cd $MODULE_DIR && cp .gitconfig ~/
+
+if [ -e ~/$GIT_CONFIG_FILE ]; then
+	mv $GIT_CONFIG_FILE $GIT_CONFIG_FILE.$timestamp
+fi
+
+git clone $GIT_REPOSITORY_URL ./$MODULE_DIR
+
+cp $MODULE_DIR/$GIT_CONFIG_FILE ~/
+
 rm -rf $MODULE_DIR
 
-echo "Done!"
+echo "DONE!"
